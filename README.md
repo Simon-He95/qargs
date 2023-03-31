8585a32bb5ce3b651c4eeb794f807b0e6984b2de
Simon He <13917107469@163.com>
2023-03-31 21:52:15 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,33 @@
+## easy-args
+Help you quickly process argv
+
+## Install
+```bash
+npm i easy-args
+```
+
+## Usage
+```bash
+import easyArgs from 'easy-args'
+
+const args = easyArgs()
+// run --help
+args.has('help') // true
+
+// run --username simon_he
+args.get('username') // simon_he
+
+// run --username simon_he --password 123456
+args.get(['username','password']) // [simon_he,123456]
+
+// run find
+args.hasCommand('find') // true
+```
+
+## License
+[MIT](./LICENSE) License © 2022 [Simon He](https://github.com/Simon-He95)
+
+<a href="https://github.com/Simon-He95/sponsor" target="_blank"><img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" style="height: 51px !important;width: 217px !important;" ></a>
+
+
+<span><div align="center">![sponsors](https://www.hejian.club/images/sponsors.jpg)</div></span>
---
+++ src/index.ts
@@ -0,0 +1,45 @@
+import type { Options } from './type'
+
+function easyArgs() {
+  const args = process.argv.slice(2)
+  const options: Options[] = []
+  const commanders: string[] = []
+
+  const len = args.length
+  for (let i = 0; i < len; i++) {
+    const arg = args[i]
+    const match = arg.match(/-{1,2}(\w+)/)
+    if (match) {
+      const name = match[1]
+      const value = args[i + 1]
+      options.push({ name, value: value.startsWith('-') ? undefined : value })
+    }
+    else {
+      commanders.push(arg)
+    }
+  }
+
+  const has = (key: string) => {
+    return options.some(o => o.name === key)
+  }
+  const get = (key: string | string[]) => {
+    if (typeof key === 'string')
+      return options.find(o => o.name === key)?.value
+    else
+      return options.filter(o => key.includes(o.name)).map(item => item.value)
+  }
+
+  const hasCommand = (key: string) => {
+    return commanders.includes(key)
+  }
+
+  return {
+    options,
+    commanders,
+    has,
+    get,
+    hasCommand,
+  }
+}
+
+export default easyArgs
